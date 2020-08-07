24cdf72f1b964408f2ff476dfea839c50ee94823
Amy Li <45214377+aamyli@users.noreply.github.com>
2020-08-07 07:06:52 -0400

    create react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Home from "./components/home.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <container-fluid id="home">
+          <div>home page</div>
+      </container-fluid>
+    );
+  }
+}
