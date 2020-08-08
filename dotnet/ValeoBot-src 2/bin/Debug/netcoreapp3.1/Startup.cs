fd007089ee705fec45391c8f7b50f168c595cdcd
Savan Malhotra <savan@notifyapp.ca>
2020-08-08 14:06:15 -0400

    organized repo

---
+++ dotnet/ValeoBot-src 2/bin/Debug/netcoreapp3.1/Startup.cs
@@ -0,0 +1,83 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.BotBuilderSamples.Bots;
+using Microsoft.BotBuilderSamples.Dialogs;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Bot.Builder.AI.QnA;
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddNewtonsoftJson();
+
+            // Create the Bot Framework Adapter with error handling enabled.
+            services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+
+            // Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.)
+            services.AddSingleton<IStorage, MemoryStorage>();
+
+            // Create the User state. (Used in this bot's Dialog implementation.)
+            services.AddSingleton<UserState>();
+
+            // Create the Conversation state. (Used by the Dialog system itself.)
+            services.AddSingleton<ConversationState>();
+
+            // Register LUIS recognizer
+            services.AddSingleton<FlightBookingRecognizer>();
+
+            // Register the BookingDialog.
+            services.AddSingleton<BookingDialog>();
+
+            // The MainDialog that will be run by the bot.
+            services.AddSingleton<MainDialog>();
+
+            // Create QnAMaker endpoint as a singleton
+            // services.AddSingleton(new QnAMakerEndpoint
+            // {
+            //     KnowledgeBaseId = Configuration.GetValue<string>($"QnAKnowledgebaseId"),
+            //     EndpointKey = Configuration.GetValue<string>($"QnAAuthKey"),
+            //     Host = Configuration.GetValue<string>($"QnAEndpointHostName")
+            // });
+             services.AddSingleton(new QnAMakerEndpoint
+            {
+                KnowledgeBaseId = "eb711951-fc65-4f8b-8b01-46f6fb063684",
+                EndpointKey = "bb782fea-de31-489a-b330-36e3e90ebbda",
+                Host = "https://valeoqna.azurewebsites.net/qnamaker"
+            });
+
+            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+            services.AddTransient<IBot, DialogAndWelcomeBot<MainDialog>>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles()
+                .UseStaticFiles()
+                .UseWebSockets()
+                .UseRouting()
+                .UseAuthorization()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+
+            // app.UseHttpsRedirection();
+        }
+    }
+}
---
+++ dotnet/ValeoBot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=.\valeobot-Web-Deploy.pubxml -p:Password=vb9n9Fs8zktRhGuWrGspnP0ZukNtJH3ygDxyhQnSbSwlWPfBpn7YRBynyoc4
+
